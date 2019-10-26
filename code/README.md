be0aa4b30ec21817e956084061d0f8995afa83dd
liwen-tj <liwen00812@qq.com>
2019-10-26 04:49:43 -0700

    generate python code done

---
+++ code/README.md
@@ -0,0 +1,4 @@
+- packages文件每行代表一个依赖包
+- generateLambda.py根据packages随机生成一些列的函数
+- lambdas文件夹下面是生成的文件
+- lambdas.json文件对应着每个函数名对应的依赖信息
---
+++ code/generateLambda.py
@@ -0,0 +1,46 @@
+packageFilePath = "packages"
+
+# 加载依赖包
+
+with open(packageFilePath) as f:
+    data = f.readlines()
+    packages = [d[:-1] for d in data]
+    print(packages)
+
+
+# 生成python文件
+def genPYFile(packs, filename):
+    # packs: 依赖列表
+    with open(filename, 'w') as f:
+        packs = ['import ' + p + '\n' for p in packs]
+        packs.append('\ndef main():\n')
+        packs.append('  return \'' + filename + ' done!\'\n')
+        f.writelines(packs)
+        
+
+
+# 生成50个函数
+import random
+import json
+from collections import OrderedDict
+
+prob = [0.3, 0.4, 0.5, 0.6, 0.7]
+i = 1
+lambda_info = OrderedDict()
+for pro in prob:
+    for _ in range(10):
+        target = []
+        for pac in packages:
+            x = random.random()
+            if x > pro:
+                target.append(pac)
+        # 生成一个文件
+        genPYFile(target, './lambdas/lambda_' + str(i) + '.py')
+        lambda_info['lambda_' + str(i)] = target
+        i += 1
+
+json_str = json.dumps(lambda_info, indent=4)
+with open('lambdas.json', 'w') as json_file:
+    json_file.write(json_str)
+
+    
